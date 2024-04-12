51138326c2df8bcd607a1a0b43eb241eb967628c
Petros Angelatos <petrosagg@gmail.com>
2024-04-12 14:19:14 +0300

    fix typo

--- foo/a1.1-system_calls.c
+++ foo/a1.1-system_calls.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]){
         }
 
         for (int i = 0; i < rcnt; i++) {
-            if (buff[j] == sc) {
+            if (buff[i] == sc) {
                 count++;
             }
         }
