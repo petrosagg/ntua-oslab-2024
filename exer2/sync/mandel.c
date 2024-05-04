a09ff09a5233291373ee74b8a96a904765299839
Petros Angelatos <petrosagg@gmail.com>
2024-05-04 17:59:59 +0300

    parallel mandlebrot solution

--- exer2/sync/mandel.c
+++ exer2/sync/mandel.c
@@ -11,10 +11,49 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <errno.h>
+#include <signal.h>
 
 #include "mandel-lib.h"
 
 #define MANDEL_MAX_ITERATION 100000
+
+#if defined(SYNC_CONDVAR) ^ defined(SYNC_SEMAPHORE) == 0
+# error You must #define exactly one of SYNC_CONDVAR or SYNC_SEMAPHORE.
+#endif
+
+#if defined(SYNC_CONDVAR)
+# define USE_CONDVARS 1
+#else
+# define USE_CONDVARS 0
+#endif
+
+#define perror_pthread(ret, msg) \
+	do { errno = ret; perror(msg); } while (0)
+
+int safe_atoi(char *s, int *val)
+{
+	long l;
+	char *endp;
+
+	l = strtol(s, &endp, 10);
+	if (s != endp && *endp == '\0') {
+		*val = l;
+		return 0;
+	} else
+		return -1;
+}
+
+void usage(char *argv0)
+{
+	fprintf(stderr, "Usage: %s thread_count\n\n"
+		"Exactly one argument required:\n"
+		"    thread_count: The number of threads to create.\n",
+		argv0);
+	exit(1);
+}
 
 /***************************
  * Compile-time parameters *
@@ -98,32 +137,140 @@ void output_mandel_line(int fd, int color_val[])
 	}
 }
 
-void compute_and_output_mandel_line(int fd, int line)
-{
-	/*
-	 * A temporary array, used to hold color values for the line being drawn
-	 */
-	int color_val[x_chars];
+// Whether we should termimnate early due to a ctrl+c
+int terminate_early = 0;
 
-	compute_mandel_line(line, color_val);
-	output_mandel_line(fd, color_val);
+void signal_handler(int signum) {
+	// Use an atomic operation to set the flag since we're in a multi-threaded
+	// context.
+	__sync_fetch_and_or(&terminate_early, 1);
 }
 
-int main(void)
+// An array of semaphores where semaphores[i] contains the semaphore
+// corresponding to the i-th thread
+sem_t *semaphores;
+pthread_mutex_t lock;
+pthread_cond_t cond;
+int cur_line = 0;
+
+// A (distinct) instance of this structure is passed to each thread
+struct thread_info_struct {
+	// The POSIX thread id, as returned by the library
+	pthread_t tid;
+	// The thread id
+	int thrid;
+	// The total number of threads
+	int thrcnt;
+};
+
+void *thread_start_fn(void *arg) {
+	struct thread_info_struct *thr = arg;
+
+	// A temporary array, used to hold color values for the line being drawn
+	int color_val[x_chars];
+
+	for (int line = thr->thrid; line < y_chars; line += thr->thrcnt) {
+		// If the user asked for termination exit early
+		if (terminate_early) {
+			break;
+		}
+
+		// Computing the line doesn't need to synchronize with other threads since
+		// it operates on a local array.
+		compute_mandel_line(line, color_val);
+
+		if (USE_CONDVARS) {
+			pthread_mutex_lock(&lock);
+			// Wait until the current line being drawn is the line we are attempting to draw
+			while (cur_line < line) {
+				pthread_cond_wait(&cond, &lock);
+			}
+		} else {
+			// Wait for this thread's semaphore
+			sem_wait(&semaphores[thr->thrid]);
+		}
+
+		// We are in the critical section so it is our turn to draw a line.
+		output_mandel_line(1, color_val);
+
+		if (USE_CONDVARS) {
+			// Advance the current line and broadcast the condition variable so that
+			// all other threads re-check it.
+			cur_line += 1;
+			pthread_cond_broadcast(&cond);
+			pthread_mutex_unlock(&lock);
+		} else {
+			// Post next thread's semaphore
+			sem_post(&semaphores[(thr->thrid + 1) % thr->thrcnt]);
+		}
+	}
+
+	return NULL;
+}
+
+int main(int argc, char* argv[])
 {
-	int line;
+	int thrcnt;
+	struct thread_info_struct *thr;
+
+	if (argc != 2)
+		usage(argv[0]);
+	if (safe_atoi(argv[1], &thrcnt) < 0 || thrcnt <= 0) {
+		fprintf(stderr, "`%s' is not valid for `thread_count'\n", argv[1]);
+		exit(1);
+	}
+
+	// Install signal handler to gracefully handle CTRL+C
+	struct sigaction sa;
+	sa.sa_flags = SA_RESTART;
+	sa.sa_handler = signal_handler;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGINT, &sa, NULL) < 0) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	thr = calloc(thrcnt, sizeof(*thr));
+
+	// If we're using semaphores we need to allocate as many as there are threads
+	if (!USE_CONDVARS) {
+		semaphores = calloc(thrcnt, sizeof(*semaphores));
+		for (int i = 0; i < thrcnt; i++) {
+			if (sem_init(&semaphores[i], 0, 0) < 0) {
+				perror("sem_init");
+				exit(1);
+			}
+		}
+		// Post the first thread so that the process starts from line 0
+		sem_post(&semaphores[0]);
+	}
 
 	xstep = (xmax - xmin) / x_chars;
 	ystep = (ymax - ymin) / y_chars;
 
-	/*
-	 * draw the Mandelbrot Set, one line at a time.
-	 * Output is sent to file descriptor '1', i.e., standard output.
-	 */
-	for (line = 0; line < y_chars; line++) {
-		compute_and_output_mandel_line(1, line);
+	// Create threads
+	for (int i = 0; i < thrcnt; i++) {
+		thr[i].thrid = i;
+		thr[i].thrcnt = thrcnt;
+
+		int ret = pthread_create(&thr[i].tid, NULL, thread_start_fn, &thr[i]);
+		if (ret) {
+			perror_pthread(ret, "pthread_create");
+			exit(1);
+		}
+	}
+
+	// Wait for all threads to terminate
+	for (int i = 0; i < thrcnt; i++) {
+		int ret = pthread_join(thr[i].tid, NULL);
+		if (ret) {
+			perror_pthread(ret, "pthread_join");
+			exit(1);
+		}
 	}
 
 	reset_xterm_color(1);
-	return 0;
+
+	// If this is not a normal shutdown we return a non-zero code
+	return terminate_early;
 }
