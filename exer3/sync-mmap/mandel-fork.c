5803b5aaeb333b3d200b298d7c391cd24fd8aaa6
Petros Angelatos <petrosagg@gmail.com>
2024-05-05 10:58:49 +0300

    solution for process sync mandel

--- exer3/sync-mmap/mandel-fork.c
+++ exer3/sync-mmap/mandel-fork.c
@@ -11,12 +11,31 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
 
 /*TODO header file for m(un)map*/
 
 #include "mandel-lib.h"
 
 #define MANDEL_MAX_ITERATION 100000
+
+#if defined(SYNC_SEMAPHORE) ^ defined(SYNC_SHAREDBUF) == 0
+# error You must #define exactly one of SYNC_SHAREDBUF or SYNC_SEMAPHORE.
+#endif
+
+#if defined(SYNC_SEMAPHORE)
+# define USE_SEMAPHORES 1
+#else
+# define USE_SEMAPHORES 0
+#endif
+
+// An array of semaphores where semaphores[i] contains the semaphore
+// corresponding to the i-th process
+sem_t *semaphores;
+// An array of colors used when we don't use semaphores for synchronization
+int* color_buf;
 
 /***************************
  * Compile-time parameters *
@@ -132,9 +151,7 @@ void *create_shared_memory_area(unsigned int numbytes)
 	pages = (numbytes - 1) / sysconf(_SC_PAGE_SIZE) + 1;
 
 	/* Create a shared, anonymous mapping for this number of pages */
-	/* TODO:  
-		addr = mmap(...)
-	*/
+	addr = mmap(NULL, pages * sysconf(_SC_PAGE_SIZE), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
 	return addr;
 }
@@ -159,19 +176,98 @@ void destroy_shared_memory_area(void *addr, unsigned int numbytes) {
 	}
 }
 
-int main(void)
+int safe_atoi(char *s, int *val)
 {
-	int line;
+	long l;
+	char *endp;
+
+	l = strtol(s, &endp, 10);
+	if (s != endp && *endp == '\0') {
+		*val = l;
+		return 0;
+	} else
+		return -1;
+}
+
+void usage(char *argv0)
+{
+	fprintf(stderr, "Usage: %s process_count\n\n"
+		"Exactly one argument required:\n"
+		"    process_count: The number of processes to create.\n",
+		argv0);
+	exit(1);
+}
+
+void worker(int proc_id, int nprocs) {
+	// A temporary array, used to hold color values for the line being drawn
+	int color_val[x_chars];
+
+	for (int line = proc_id; line < y_chars; line += nprocs) {
+		if (USE_SEMAPHORES) {
+			compute_mandel_line(line, color_val);
+			// Wait for this process' semaphore
+			sem_wait(&semaphores[proc_id]);
+			// We are in the critical section so it is our turn to draw a line.
+			output_mandel_line(1, color_val);
+			// Post next process' semaphore
+			sem_post(&semaphores[(proc_id + 1) % nprocs]);
+		} else {
+			compute_mandel_line(line, &color_buf[line * x_chars]);
+		}
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	int nprocs;
+
+	if (argc != 2)
+		usage(argv[0]);
+	if (safe_atoi(argv[1], &nprocs) < 0 || nprocs <= 0) {
+		fprintf(stderr, "`%s' is not valid for `process_count'\n", argv[1]);
+		exit(1);
+	}
+
+	// If we're using semaphores we need to allocate as many as there are processes
+	if (USE_SEMAPHORES) {
+		semaphores = create_shared_memory_area(nprocs * sizeof(*semaphores));
+		for (int i = 0; i < nprocs; i++) {
+			if (sem_init(&semaphores[i], 1, 0) < 0) {
+				perror("sem_init");
+				exit(1);
+			}
+		}
+		// Post the first process so that the process starts from line 0
+		sem_post(&semaphores[0]);
+	} else {
+		color_buf = create_shared_memory_area(x_chars * y_chars * sizeof(int));
+	}
 
 	xstep = (xmax - xmin) / x_chars;
 	ystep = (ymax - ymin) / y_chars;
 
-	/*
-	 * draw the Mandelbrot Set, one line at a time.
-	 * Output is sent to file descriptor '1', i.e., standard output.
-	 */
-	for (line = 0; line < y_chars; line++) {
-		compute_and_output_mandel_line(1, line);
+	// Create processes
+	for (int i = 0; i < nprocs; i++) {
+		int p = fork();
+		if (p < 0) {
+			perror("fork");
+			exit(1);
+		}
+		if (p == 0) {
+			worker(i, nprocs);
+			return 0;
+		}
+	}
+
+	// Wait for all processes to terminate
+	for (int i = 0; i < nprocs; i++) {
+		wait(NULL);
+	}
+
+	if (!USE_SEMAPHORES) {
+		for (int line = 0; line < y_chars; line += 1) {
+			output_mandel_line(1, &color_buf[line * x_chars]);
+		}
 	}
 
 	reset_xterm_color(1);
